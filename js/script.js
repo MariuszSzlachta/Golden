4080cd3b46fb970620327d36de9c11f3a8b959ee
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-05-30 21:22:51 +0200

    Relece candidate 1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 // hamburger menu na media queries
+var $nav = $('.nav');
 var $menu = $('.main-nav');
 var $hamburger = $('.hamburger');
 
 $hamburger.on('click', function(){
-  $menu.toggleClass('isActive');
+  $menu.toggleClass('isActive fadeInDown');
 });
 
-// animations
+// wykryć kiedy navbar jest na ilustam pikselach tak by dodawly mu sie style na background i box-shadow
+function toggleNavStyles(){
+  if (pageYOffset >= 630){
+    $nav.addClass('dark');
+  } else {
+    $nav.removeClass('dark');
+  }
+}
+document.addEventListener('scroll', toggleNavStyles); // nie wiem czy to dobra praktyka
 
-// menu po zaladowaniu strony
-$(document).ready(function(){
-  $menu.toggleClass('animation-fade fade-top');
-})
-
-// services
-var $servicesCards = $('.services-cards');
-
-inView('.services-cards').on('enter', $servicesCards.addClass('animation-fade fade-bottom'));
-
-// portfolio
+// smooth scrool
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
